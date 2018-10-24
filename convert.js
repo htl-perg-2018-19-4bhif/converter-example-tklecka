dac340371d27ee7d1f9a915b80b212251e20b44e
tklecka <tim@klecka.at>
2018-10-24 21:58:28 +0200

    Covert added (Only 10 Lines)

--- convert.js
+++ convert.js
@@ -1,1 +1,10 @@
-console.log('Not implemented yet!');
+const args = process.argv;
+if(process.argv.length===6 && process.argv[4]==="to" && !isNaN(process.argv[2])){
+    args[3] === args[5] ? print(1) : args[3] === "m" && args[5] === "cm" ? print(100) : args[3] === "m" && args[5] === "mm" ? print(1000) : args[3] === "cm" && args[5] === "m" ? print(0.01) : args[3] === "cm" && args[5] === "mm" ? print(10) : args[3] === "mm" && args[5] === "cm" ? print(0.1) : args[3] === "mm" && args[5] === "m" ? print(0.001) : args[3] === "kg" && args[5] === "g" ? print(1000) : args[3] === "g" && args[5] === "kg" ? print(0.001) : console.log("Invalid parameters");
+} else {
+    console.log("Invalid parameters");
+}
+function print(factor){
+    console.log(args[2] + " " + args[3] + " are " + args[2]*factor + " " + args[5]);
+}
+//Still one Left for an Comment :D
